d1cf6467a2891686211b8ed69419518beb9fe9b4
Juan Jose Garcia Lau <juanjo.garcia@gmail.com>
2022-08-11 14:51:10 +0200

    : Initial commit

---
+++ src/test/java/org/juanjo/playground/MoneyTest.java
@@ -0,0 +1,44 @@
+package org.juanjo.playground;
+
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MoneyTest {
+
+    private static final Long LONG_AMOUNT = 12345L;
+    private static String CURRENCY = "EUR";
+
+    private Money eur(double amount) {
+        return Money.of(amount, CURRENCY);
+    }
+
+    @Test
+    void testMoneyRoundingWithoutScale() {
+        var money = Money.of(
+                new BigDecimal(LONG_AMOUNT)
+                        .divide(BigDecimal.valueOf(100L), RoundingMode.HALF_EVEN), CURRENCY);
+        var expected = eur(LONG_AMOUNT / 100d);
+        System.out.println("money = " + money);
+        System.out.println("expected = " + expected);
+        // without scale, decimal data is lost
+        assertThat(money)
+                .isNotEqualTo(expected)
+                .isEqualTo(eur(123.00d));
+    }
+
+    @Test
+    void testMoneyRoundingWithScale() {
+        var money = Money.of(
+                new BigDecimal(LONG_AMOUNT)
+                        .divide(BigDecimal.valueOf(100L), 4, RoundingMode.HALF_EVEN), CURRENCY);
+        var expected = eur(LONG_AMOUNT / 100d);
+        System.out.println("money = " + money);
+        System.out.println("expected = " + expected);
+        assertThat(money).isEqualTo(expected);
+    }
+}
