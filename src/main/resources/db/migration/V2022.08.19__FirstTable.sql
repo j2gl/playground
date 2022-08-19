ac74c45d8750ea00682f03e2710c8caca2c2f4b5
Juan Jose Garcia Lau <juanjo.garcia@gmail.com>
2022-08-19 18:39:09 +0200

    master: master: Remove flyway

--- src/main/resources/db/migration/V2022.08.19__FirstTable.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE transaction (
-    id int not null primary key,
-    description varchar,
-    transaction_date timestamp with time zone not null,
-    created_at timestamp with time zone not null,
-    updated_at timestamp with time zone not null,
-    version integer not null
-);
