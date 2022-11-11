6654070b4be3f621da5985ee2aea3b3bc7de6014
Juan Jose Garcia Lau <juanjo.garcia@gmail.com>
2022-11-11 11:55:11 +0100

    master: Add repeated test

---
+++ src/test/java/org/juanjo/playground/RepeatedFlakyTest.java
@@ -0,0 +1,17 @@
+package org.juanjo.playground;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class RepeatedFlakyTest {
+
+    @RepeatedTest(value = 10)
+    void checkNumberIsEven_flakyTest() {
+        int number = (int) (Math.random() * 1000);
+        log.info("Is {} an even number?", number);
+        assertThat(number % 2).isZero();
+    }
+}
