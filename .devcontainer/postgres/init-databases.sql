d095dd57e602f2d069e0b9e4fb5ace2cf5a74dc5
Juan Jose Garcia Lau <j2gl@users.noreply.github.com>
2023-01-10 21:10:22 +0000

    Add persmissions

--- .devcontainer/postgres/init-databases.sql
+++ .devcontainer/postgres/init-databases.sql
@@ -1,4 +1,7 @@
 
 CREATE DATABASE playground;
 CREATE USER devtools WITH PASSWORD 'DEVELOPMENT';
+
 GRANT ALL PRIVILEGES ON DATABASE playground TO devtools;
+GRANT ALL ON SCHEMA public TO devtools;
+GRANT USAGE ON SCHEMA public TO devtools;
